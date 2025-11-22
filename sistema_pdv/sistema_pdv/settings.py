4d6e75800445abb0f5c0964c74b9751f9f9d2963
Alessandra <alessandravitoria799@gmail.com>
2025-11-22 14:01:19 -0300

    Ajustes no settings para recuperação de senha

--- sistema_pdv/sistema_pdv/settings.py
+++ sistema_pdv/sistema_pdv/settings.py
@@ -96,3 +96,10 @@ SESSION_SAVE_EVERY_REQUEST = False
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'seu-email@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'sua-senha-app')
